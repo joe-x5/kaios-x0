f3dc8ff6e0090795eb1504f2939975675363447d
joe-x5 <Joesmith1052024@gmail.com>
2026-06-28 12:27:54 +0900

    Update apps/Discord-kaios/index.js

---
+++ apps/Discord-kaios/index.js
@@ -0,0 +1,4 @@
+// this file is only for node.js
+let { DiscordXHR } = require("./js/DiscordXHR.js");
+let { DiscordGateway } = require("./js/DiscordGateway.js");
+Object.assign(exports, { DiscordXHR, DiscordGateway });
